28ee27a36e709465d80f31f04a452eed9082ab72
mcpeaps_HD <mcpeaps_HD@outlook.com>
2023-11-09 08:59:17 +0100

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://comboomPunkTsucht.github.io/fex-projekt',
-  base: '/docs',
+  site: 'https://comboomPunkTsucht.github.io',
+  base: '/fex-projekt',
 })
